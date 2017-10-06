bda1c3388ef1481086ab5bc0cc9c18dcbd3a0d54
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-10-06 09:58:49 +0100

    Upload of initial Index.php
    Uploaded initial files for GitHub to work with Azure.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Josh's PHP Web App</title>
+</head>
+
+<body>
+
+<?php
+
+echo "My first time trying PHP!";
+
+//This is me testing this site.
+
+?>
+
+</body>
+
+</html>
